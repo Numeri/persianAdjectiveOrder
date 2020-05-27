ff63a075e117b6b9bed5531da4de8fbbb93254ef
Numeri <Numeri@users.noreply.github.com>
2020-05-27 16:58:30 -0600

    Added support for multiple files

--- analyze.py
+++ analyze.py
@@ -1,4 +1,14 @@
 #!/bin/python3 -i
+"""Analyze Persian adjective use
+
+Usage:
+  analyze.py <file>...
+  analyze.py -h
+
+Options:
+  -h --help                       Show this screen.
+
+"""
 
 import collections
 import csv
@@ -6,6 +16,7 @@ import hazm
 import pandas
 import sys
 
+from docopt import docopt
 from typing import Counter, Dict, List, Tuple
 
 def collect_adjective_chains(sentences : List[str]):
@@ -64,16 +75,20 @@ def analyze_adjective_position(adj_chains : Dict[int, Counter[str]]):
     df = pandas.DataFrame(columns=['chain-length', 'position', 'frequency', 'word'], data=([cl, p, f, w] for (cl, p, w), f in data.items()))
     return df, data
 
-#TODO: Allow for multiple files
-if len(sys.argv) != 2:
-    print("Expected a filename")
-    sys.exit(1)
+arguments = docopt(__doc__, version='Persian Adjective Analysis 0.1')
 
+filenames = arguments['<file>']
 corpus_text = ""
 
-with open(sys.argv[1]) as infile:
-        corpus_text = '\n'.join(infile.readlines())
+for i, name in enumerate(filenames):
+    with open(name) as infile:
+            corpus_text += '\n'.join(infile.readlines())
+            print(f'Finished reading file {i+1}/{len(filenames)}:\t{name}')
 
+print('Tokenizing sentences: ', end='')
 sentences = hazm.sent_tokenize(corpus_text)
+print(f'{len(sentences)} total')
+
+print('Analyzing adjective usage...')
 adj_chains = collect_adjective_chains(sentences)
 
